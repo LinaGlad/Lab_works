183078e5748eb16dbbf4a60e4d36c5c00f478266
Kupitmanchik <igork7102@gmail.com>
2025-03-12 04:32:57 +0000

    Изменение4

--- circle.cpp
+++ circle.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <iomanip> 
-const double pi = 3.14; 
+#include <locale.h>
+const double pi = 3.14;
 using namespace std;
 
 int main() {
-	float rad, a; 
+	float rad, a;
 	cout << "Enter radius ";
-	cin >> rad; 
+	cin >> rad;
 	cout << "Enter seqtor ";
-	cin >> a; 
-	float lenght = 2 * pi * rad; 
-	float square = pi * rad * rad; 
-	float square_sector = square * a / 360; 
-	cout << fixed << setprecision(2) << lenght << endl; 
-	cout << fixed << setprecision(2) << square << endl; 
-	cout << fixed << setprecision(2) << square_sector << endl; 
+	cin >> a;
+	if (rad > 0 and a > 0, a<=360) {
+		float lenght = 2 * pi * rad;
+		float square = pi * rad * rad;
+		float square_sector = square * a / 360;
+		cout << fixed << "lenght: " << setprecision(2) << lenght << endl;
+		cout << fixed << "square: " << setprecision(2) << square << endl;
+		cout << fixed << "square_sector: " << setprecision(2) << square_sector << endl;
+	}
+	else {
+		cout << fixed << "lenght: mistake" << endl;
+		cout << fixed << "square: mistake" << endl;
+		cout << fixed << "square_sector: mistake" << endl;
+	}
 }
